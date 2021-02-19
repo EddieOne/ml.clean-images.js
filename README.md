615ac111d50a2528eb92718c0ff25ceb3c56e48b
+EddieJ <eddie@onefoldmedia.com>
2021-02-19 16:28:53 -0700

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ml.clean-images.js
+
+CLI tool that walks a directory of images to find currupted files
+
+- Install NodeJs
+- Run `npm i walk jimp`
+- Run `node ml.clean-images.js`
+
+Useful for Tensorflow Image errors like Invalid JPEG data or crop window, data size 
---
+++ ml.clean-images.js
@@ -0,0 +1,21 @@
+// CLI tool that walks a directory of images to find currupted files
+
+var fs = require('fs');
+var walk = require('walk');
+var jimp = require('jimp');
+var input = process.argv[2];
+
+var walker  = walk.walk(input, { followLinks: false });
+var count = 0;
+
+walker.on('file', function(root, stat, next) {
+  jimp.read(root + '/' + stat.name, (err, image) => {
+    if (err) {
+      fs.unlinkSync(root + '/' + stat.name);
+      console.log('Deleting ' + stat.name)
+    }
+    console.log(count);
+    count++;
+    next();
+  });
+});
